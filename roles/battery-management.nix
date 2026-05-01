7588cda466d8b5c8b71b1adc8fc9c627fdec3791
fkozmik <florian.kozmik@gmail.com>
2026-05-01 10:01:21 +0200

    feat: inverse battery management logic

--- roles/battery-management.nix
+++ roles/battery-management.nix
@@ -10,7 +10,7 @@
                 CPU_BOOST_ON_BAT = 0;
                 CPU_SCALING_GOVERNOR_ON_AC = "performance";
                 CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-                START_CHARGE_THRESHOLD = 55;
+                STOP_CHARGE_THRESHOLD = 80;
             };
         };
     system76-scheduler.settings.cfsProfiles.enable = true;
